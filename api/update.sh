b91e0f1728432f748cd4a734600fb0f0ecd2f053
cathook <b01902109@csie.ntu.edu.tw>
2015-05-18 20:26:37 +0800

    Adds the auto update shell file.
    So we just need to run the command `./update.sh` and rebase all things
    to the newest agar.io's js code.

---
+++ api/update.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env bash
+#
+# Fetches the newest js code from agar.io and beautifier it.
+
+HOSTNAME='agar.io'
+
+
+beautify() {
+  src="$1"
+  dst="$2"
+  tmp="${dst}.$$"
+  for fn in $src*; do
+    cp -f "${fn}" "${tmp}"
+    js-beautify --indent-size=2 --end-with-newline "${tmp}" > "${dst}"
+    rm "${tmp}"
+  done
+}
+
+
+main() {
+  version="$(date '+%Y_%m_%d__%H_%M_%S')"
+  git checkout api_init_point
+
+  rm -rf "${HOSTNAME}"
+  wget -r "${HOSTNAME}"
+
+  beautify "${HOSTNAME}/main_out.js" main.js
+
+  rm -rf "${HOSTNAME}"
+
+  git add main.js
+  git commit --allow-empty -m "update to ${version}"
+
+  git checkout master
+  git rebase api_init_point
+}
+
+
+main "$@"
