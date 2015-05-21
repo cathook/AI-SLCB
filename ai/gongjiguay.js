d9ea86937beae06024573c530a9d8107242ce236
sycLin <b01902044@ntu.edu.tw>
2015-05-22 00:41:41 +0800

    +gongjiguay agent: add code structure

---
+++ ai/gongjiguay.js
@@ -0,0 +1,25 @@
+var ai = ai || {}; //!< namespace ai
+
+ai.gongjiguay = ai.gongjiguay || {}; //!< namespace ai.gongjiguay
+
+//! Starts to use this agent.
+ai.gongjiguay.start = function() {
+	;
+};
+
+
+//! Stops using this agent.
+ai.gongjiguay.stop = function() {
+	;
+};
+
+// need some arguments?
+ai.gongjiguay.getAttackTargetPosition = function() {
+	;
+};
+
+ai.gongjiguay._run = function() {
+	;
+};
+
+ai.gongjiguay._targetPoint = null;
