0035053340d6d8c95ebefc53365212202d3f0c87
cathook <b01902109@csie.ntu.edu.tw>
2015-05-25 04:47:37 +0800

    refactor 3

--- ai/gongjiguay.js
+++ ai/gongjiguay.js
@@ -1,26 +1,22 @@
 var ai = ai || {}; //!< namespace ai
 
-ai.gongjiguay = ai.gongjiguay || {}; //!< namespace ai.gongjiguay
 
-//! Starts to use this agent.
-ai.gongjiguay.start = function() {
-	;
+ai.GongJiGuay = function() {
+  this._targetPoint = new api.MarkPoint(new api.Position(),
+                                        '#006600', api.MarkPoint.Type.TARGET);
 };
 
 
-//! Stops using this agent.
-ai.gongjiguay.stop = function() {
-	ai.gongjiguay._runFlag = false;
-	api.removeMark(ai.gongjiguay._targetPoint);
+ai.GongJiGuay.prototype.init = function() {
+  api.addMark(this._targetPoint);
 };
+
+ai.GongJiGuay.prototype.cleanup = function() {
+  api.removeMark(this._targetPoint);
+};
+
+
+ai.GongJiGuay.prototype.run = function() {};
 
 // need some arguments?
-ai.gongjiguay.getAttackTargetPosition = function() {
-	;
-};
-
-ai.gongjiguay._run = function() {
-	;
-};
-
-ai.gongjiguay._targetPoint = null;
+ai.GongJiGuay.getAttackTargetPosition = function() {};
